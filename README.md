1915c4be510d595a81549166c7d9271ac24c3a0c
Wagner Sartori Junior <wsartori@comscore.com>
2015-12-03 12:14:50 +0100

    Adding setuptools files to a proper building/installation

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ racktables-py-client
 Python library for Racktables API
 
 Racktables API is available on https://github.com/ibettinger/racktables
-Note that you only need to copy wwwroot/api.php to your current installation.
+
+Note that you only need to copy wwwroot/api.php to your current installation
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+readme = open('README.md').read()
+
+# get the requirements from the requirements.txt
+requirements_file = open('requirements.txt').read().split()
+requirements = requirements_file
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='racktables',
+    version='0.0.1',
+    description='Racktables API python wrapper',
+    long_description=readme,
+    author='Wagner Sartori Junior',
+    author_email='wsartori@comscore.com',
+    url='https://github.com/trunet/racktables-py-client',
+    packages=[
+        'racktables',
+    ],
+    package_dir={'racktables':
+                 'racktables'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="In-house",
+    zip_safe=False,
+    keywords='racktables',
+    entry_points={
+        'console_scripts': [
+            # enable this to automatically generate a script in /usr/local/bin called myscript that points to your
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
