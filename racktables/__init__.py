1ea4e0407b2c5a75177ccb40ff70c1712b3e4009
Wagner Sartori Junior <wsartori@comscore.com>
2015-12-03 13:22:32 +0100

    fixing filenames

---
+++ racktables/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Wagner Sartori Junior'
+__email__ = 'wsartori@comscore.com'
+__version__ = '0.0.2'
+
+from racktables import *
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name='racktables',
-    version='0.0.1',
+    version='0.0.2',
     description='Racktables API python wrapper',
     long_description=readme,
     author='Wagner Sartori Junior',
